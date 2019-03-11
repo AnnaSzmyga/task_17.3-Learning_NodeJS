37925edae255213797bee654dda6cda521c1bd0c
Anna Szmyga <anna.szmyga@o2.pl>
2019-03-11 08:40:50 +0100

    Remove node_modules

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 
-process.stdin.setEncoding('utf-8');
+
+var EventEmitter = require('events').EventEmitter;
 var OSinfo = require('./modules/OSinfo');
 
+var emitter = new EventEmitter();
+emitter.on("beforeCommand", function (instruction) {
+    console.log('You wrote: ' + instruction + ', trying to run command');
+});
+emitter.on("afterCommand", function () {
+    console.log('Finished command');
+});
+
+process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
+        // odpalanie zdarzenia beforeCommand (z parametrem)
+        emitter.emit('beforeCommand', instruction);
         switch (instruction) {
             case '/exit':
                 process.stdout.write('Quitting app!\n');
@@ -23,7 +35,9 @@ process.stdin.on('readable', function() {
                 break;
             default:
                 process.stdout.write('Wrong instruction!\n');
-        }
+        };
+        // emitowanie zdarzenia afterCommand (bez parametru)
+        emitter.emit('afterCommand');
     }
 });
 
